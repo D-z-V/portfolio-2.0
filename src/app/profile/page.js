7f92ede4c95e5365ced705e06ac38793c91d7704
Dev Bhandari <dev.bhandari@hotmail.com>
2024-01-09 12:44:17 +0530

    background colour fix

---
+++ src/app/profile/page.js
@@ -0,0 +1,24 @@
+"use client";
+
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Avatar, Button, CardActionArea, CardActions, IconButton, Collapse } from '@mui/material';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Box, CssBaseline } from '@mui/material';
+import ProfileIcon from '@/Icons/ProfileIcon';
+import ShareIcon from '@/Icons/ShareIcon';
+import CommentIcon from '@/Icons/CommentIcon';
+import Navbar from '@/components/Navbar';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Reel from '@/components/Reel';
+
+const profile = () => {
+    return (
+        <></>
+    )
+}
+
+export default profile;
